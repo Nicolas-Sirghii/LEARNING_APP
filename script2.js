bb4a39821591d542ae95f887f877defae44bbb3b
Nicolas <nicolas.mailbox100@gmail.com>
2024-12-20 16:41:29 +0100

    close to be done

--- script2.js
+++ script2.js
@@ -1,3 +1,98 @@
+const answerdata = {
+    70: "a",
+    69: "a",
+    68: "a",
+    67: "a",
+    66: "a",
+    65: "william shakespeare",
+    64: "a",
+    63: "a",
+    62: "a",
+    61: "a",
+    60: "a",
+    59: "a",
+    58: "a",
+    57: "a",
+    56: "a",
+    55: "a",
+    54: "a",
+    53: "a",
+    52: "a",
+    51: "a",
+    50: "a",
+    49: "a",
+    48: "a",
+    47: "tennis",
+    46: "a",
+    45: "a",
+    44: "algeria",
+    43: "a",
+    42: "a",
+    41: "a",
+    40: "a",
+    39: "a",
+    38: "a",
+    37: "a",
+    36: "a",
+    35: "a",
+    34: "a",
+    33: "1492",
+    32: "a",
+    31: "a",
+    30: "a",
+    29: "a",
+    28: "a",
+    27: "a",
+    26: "a",
+    25: "a",
+    24: "a",
+    23: "a",
+    22: "a",
+    21: "a",
+    20: "a",
+    19: "a",
+    18: "a",
+    17: "a",
+    16: "a",
+    15: "a",
+    14: "a",
+    13: "a",
+    12: "a",
+    11: "a",
+    10: "a",
+    9: "a",
+    8: "a",
+    7: "a",
+    6: "a",
+    5: "the kangaroo",
+    4: "venus",
+    3: "a",
+    2: "a",
+    1: "a",
+    
+    
+   
+  };
+
+  const getSimpleRandom = (n)=>{
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+        arr.push(i);
+    }
+  
+    // Shuffle the array using Fisher-Yates algorithm
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));  // Random index between 0 and i
+        [arr[i], arr[j]] = [arr[j], arr[i]];  // Swap arr[i] and arr[j]
+    }
+    firstArray = arr.slice(0, 5);   // Index 0 to 4
+    secondArray = arr.slice(5, 20);  // Index 5 to 9
+    thirdArray = arr.slice(20, 70); // Index 10 to 29
+  };
+
+
+
+
 const firstCont = document.getElementById('firstCont');
 const secondCont = document.getElementById('secondCont');
 const thirdCont = document.getElementById('thirdCont');
@@ -33,6 +128,7 @@ let turn = 1;
 
 
 
+let finish = 0;
 
 
 
@@ -43,11 +139,31 @@ let turn = 1;
 
 
 
+let firstArray = [44, 33, 47, 65, 5];
+let secondArray = [4, 7, 70 ,8, 10, 1, 12, 66, 56, 15, 16, 43, 66, 19, 20];
+let thirdArray = [33,34, 57, 29, 40, 11, 39, 60, 39, 59, 16, 17, 18, 19, 20 , 33, 34, 37, 29, 40, 11, 39, 60,
+ 39, 59, 16, 17, 18, 19, 2,33,34, 27, 29, 40, 11, 39, 60, 39, 59, 16, 17, 18, 19, 2];
+ getSimpleRandom(65);
 
-const firstArray = [44, 33, 47, 65, 5];
-const secondArray = [4, 7, 74 ,8, 10, 0, 12, 66, 76, 15, 76, 43, 66, 19, 20];
-const thirdArray = [33,34, 87, 29, 40, 11, 39, 60, 39, 59, 16, 17, 18, 19, 20 , 33,34, 87, 29, 40, 11, 39, 60,
- 39, 59, 16, 17, 18, 19, 2,33,34, 87, 29, 40, 11, 39, 60, 39, 59, 16, 17, 18, 19, 2];
+ startButton.addEventListener('click', () => {
+    currentContainerIndex = 0;
+    currentIndexInContainer = 0;
+    blueDivs.clear();
+    highlightDiv(currentContainerIndex, currentIndexInContainer);
+
+
+    videoQuestion.src = `./questions/question${dd}.mp4`
+    videoQuestion.style.zIndex = '3';
+     videoAnswer.style.zIndex = '-1';
+     information.style.zIndex = '-1';
+     
+     videoQuestion.addEventListener('ended',() => {
+      videoQuestion.style.zIndex = '-4';
+      information.style.zIndex = '1';
+     controls.style.zIndex = "3"
+      
+      })
+});
 
 // Create divs in each container
 function createDivs(container, array) {
@@ -73,12 +189,14 @@ let currentContainerIndex = 0;
 let currentIndexInContainer = 0;
 let blueDivs = new Set();
 let dd = 0
+let prev = []
 const highlightDiv = (containerIndex, indexInContainer) => {
     containers.forEach(container => container.forEach(div => div.style.border = '1px solid white'));
     const currentDiv = containers[containerIndex][indexInContainer];
     dd = currentDiv.textContent
+    prev.push(answerdata[dd])
     currentDiv.style.border = '3px solid red';
-    console.log(`Div number: ${dd}`);
+    
 }
 
 function getNextDivIndex(containerIndex, indexInContainer) {
@@ -96,41 +214,7 @@ function getNextDivIndex(containerIndex, indexInContainer) {
     return nextIndex;
 }
 
-startButton.addEventListener('click', () => {
-    currentContainerIndex = 0;
-    currentIndexInContainer = 0;
-    blueDivs.clear();
-    highlightDiv(currentContainerIndex, currentIndexInContainer);
 
-
-    videoQuestion.src = `./questions/question${dd}.mp4`
-    videoQuestion.style.zIndex = '3';
-     videoAnswer.style.zIndex = '-1';
-     information.style.zIndex = '-1';
-     
-     videoQuestion.addEventListener('ended',() => {
-      videoQuestion.style.zIndex = '-4';
-      information.style.zIndex = '1';
-     controls.style.zIndex = "3"
-      
-      })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
 
 let theWord = "word"
 
@@ -145,8 +229,8 @@ input.addEventListener('keypress', event => {
         const currentDiv = currentContainer[currentIndexInContainer];
          theWord = value.toLowerCase()
 
-
-        if (value.toLowerCase() === 'yes') {
+             
+        if (value.toLowerCase() == answerdata[dd]) {
             currentDiv.style.backgroundColor = 'blue';
             blueDivs.add(currentDiv);
 
@@ -155,27 +239,28 @@ input.addEventListener('keypress', event => {
               rightAnswer.style.zIndex = "-2"
             }, 1000);
         
-                 
+                 finish ++
+
+
             videoAnswer.src = `./answers/answer${dd}.mp4`
             videoAnswer.style.zIndex = '3';
             information.style.zIndex = '-1';
+
             
-            videoAnswer.addEventListener('ended',() => {
-              videoAnswer.style.zIndex = '-1';
-              videoQuestion.style.zIndex = '3';
-             videoQuestion.src = `./questions/question${dd}.mp4`
-             
-             
-              });
+                videoAnswer.addEventListener('ended',() => {
 
-              
-            } 
-            
-                
-                
-
-
-        let nextIndex = getNextDivIndex(currentContainerIndex, currentIndexInContainer);
+                    if (finish < 64) {
+                        videoAnswer.style.zIndex = '-1';
+                        videoQuestion.style.zIndex = '3';
+                       videoQuestion.src = `./questions/question${dd}.mp4`
+                    } else {
+                        theEnd.style.zIndex = "4"
+                    }
+                  
+                   });
+        
+             } 
+            let nextIndex = getNextDivIndex(currentContainerIndex, currentIndexInContainer);
         if (nextIndex === null) {
             // Move to the next container if all divs in the current container are blue
             let allBlue = true;
@@ -187,7 +272,7 @@ input.addEventListener('keypress', event => {
             }
 
             if (allBlue) {
-                console.log('The end');
+                finish = true
                 return;
             }
 
@@ -206,29 +291,29 @@ input.addEventListener('keypress', event => {
 
         highlightDiv(currentContainerIndex, currentIndexInContainer);
 
-
-        if (theWord !== "yes") {
-            wrongAnswer.style.zIndex = "4"
-            setTimeout(() => {
-              wrongAnswer.style.zIndex = "-1"
-            }, 1000);
-          
-          videoQuestion.src = `./questions/question${dd}.mp4`
-          
-          
-          videoQuestion.style.zIndex = '3';
-          videoAnswer.style.zIndex = '-1';
-          information.style.zIndex = '-1';
-            
-          videoQuestion.addEventListener('ended',() => {
-            videoQuestion.style.zIndex = '-1';
-            information.style.zIndex = '3';
-            controls.style.zIndex = "4"
-            
-            })
-        } 
+            if (theWord !== prev[prev.length - 2]) {
+                wrongAnswer.style.zIndex = "4"
+                setTimeout(() => {
+                  wrongAnswer.style.zIndex = "-1"
+                }, 1000);
+              
+              videoQuestion.src = `./questions/question${dd}.mp4`
+              
+              
+              videoQuestion.style.zIndex = '3';
+              videoAnswer.style.zIndex = '-1';
+              information.style.zIndex = '-1';
+                
+              videoQuestion.addEventListener('ended',() => {
+                videoQuestion.style.zIndex = '-1';
+                information.style.zIndex = '3';
+                controls.style.zIndex = "4"
+                
+                })
+            }  
+        
+       
     }
-    
     
 
     
